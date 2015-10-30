16b62e280f089e8853041bd65bd296cd26772edd
dime <dime@danwi.me>
2015-10-30 17:32:21 +0800

    引用模块，　接受请求

---
+++ help.js
@@ -0,0 +1,28 @@
+
+function getType(endTag){
+    var type = null;
+    switch (endTag) {
+        case 'html' :
+        case 'htm' :
+            type = 'text/html; charset=UTF-8';
+            break;
+        case 'js' :
+            type = 'application/javascript; charset="UTF-8"';
+            break;
+        case 'css' :
+            type = 'text/css; charset="UTF-8"';
+            break;
+        case 'txt' :
+            type = 'text/plain; charset="UTF-8"';
+            break;
+        case 'manifest' :
+            type = 'text/cache-manifest; charset="UTF-8"';
+            break;
+        default :
+            type = 'application/octet-stream';
+            break;
+    }
+    return type;
+}
+
+module.exports.getType1111 = getType;
--- httpserver.js
+++ httpserver.js
@@ -1,13 +1,38 @@
 var port = 3000;
 var http = require("http");
+var fs = require("fs");
 var count = 0;
+
+var help = require("./help.js");
 
 var server = http.createServer(function (request, response) {
     count++;
     console.log("request:"+count);//请求计数
 
-
     var url = require('url').parse(request.url);
+
+    switch (url.pathname){
+        case "/check.do":
+            console.log("login.do is coming");
+            break;
+        default://加载本地文件
+            var filename = url.pathname.substring(1);    // 去掉前导'/'
+            var type = help.getType1111(filename.substring(filename.lastIndexOf('.')+1));
+            // 异步读取文件,并将内容作为单独的数据模块传给回调函数
+            // 对于确实很大的文件,使用流API fs.createReadStream()更好
+            fs.readFile(filename, function(err, content){
+                if(err) {
+                    response.writeHead(404, { 'Content-Type':'text/plain; charset="UTF-8"' });
+                    response.write("404, 没有这个页面！");
+                    response.end();
+                } else {
+                    response.writeHead(200, { 'Content-Type' : type });
+                    response.write(content);
+                    response.end();
+                }
+            });
+            break;
+    }
 
 });
 server.listen(port);
